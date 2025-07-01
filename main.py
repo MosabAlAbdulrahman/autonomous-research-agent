7014e5f9ef487395630c0f820cfa12923e8806c3
Mosab Al-Abdulrahman <83370427+MosabAlAbdulrahman@users.noreply.github.com>
2025-07-01 23:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-# Entry point to run the pipeline
+from scraping.scraper import scrape_techcrunch_ai_articles
+from models.load_local_model import load_model
+from agents.analyst_agent import analyze_articles
+from reports.generate_pdf import generate_report
 
-if __name__ == '__main__':
-    print('Pipeline starting...')
+if __name__ == "__main__":
+    print("Starting Autonomous Research Agent...")
+
+    articles = scrape_techcrunch_ai_articles()
+    model = load_model()
+    summaries = analyze_articles(articles, model)
+    generate_report(summaries)
+
+    print("Report generated at: reports/weekly_report.pdf")
